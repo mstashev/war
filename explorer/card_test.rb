cdcd3653f021301cd80a9e4e0241f6a641f7038d
Michael Stashevsky <mstashev@gmail.com>
2017-02-08 12:15:47 -0500

    Started Explorer Mode

---
+++ explorer/card_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative 'card'
+
+class CardTest < MiniTest::Test
+
+end
---
+++ explorer/deck_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+require_relative 'deck'
+
+class DeckTest < MiniTest::Test
+
+
+end
